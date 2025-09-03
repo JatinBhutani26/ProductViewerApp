88ca8f7e395af1eeb619d2ec75bc894fbd75cdca
Jatin Bhutani <Jatin.Bhutani2626@gmail.com>
2025-09-03 19:40:12 +0530

    Adding debug route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Config;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,6 +18,26 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('/debug-db', function () {
+    $status = null;
+
+    try {
+        DB::connection()->getPdo();
+        $status = '✅ Connection OK';
+    } catch (\Exception $e) {
+        $status = '❌ ' . $e->getMessage();
+    }
+
+    return response()->json([
+        'DB_HOST'     => env('DB_HOST'),
+        'DB_DATABASE' => env('DB_DATABASE'),
+        'DB_USERNAME' => env('DB_USERNAME'),
+        'DB_SSLMODE'  => env('DB_SSLMODE'),
+        'DB_CONFIG'   => Config::get('database.connections.pgsql'),
+        'DB_CHECK'    => $status,
+    ]);
 });
 
 Route::get('/products', [ProductController::class, 'index'])->middleware('auth');
